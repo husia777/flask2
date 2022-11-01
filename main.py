f24a4bc75c7d783ac393337367b29dfa22fbd6aa
husia777 <naimovsarif411@gmail.com>
2022-11-01 17:12:37 +0500

    init

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template,request
+
+app = Flask(__name__)
+
+
+@app.route('/<users>/<id>')
+def users(users, id):
+    return render_template('users/show.html', id=id, nickname=users)
+
+
+@app.route('/users')
+def get_users():
+    users = ['mike', 'mishel', 'adel', 'keks', 'kamila']
+    return render_template('users/index.html', users=users)
+
+
+@app.route('/users')
+def filter_users():
+    users = ['mike', 'mishel', 'adel', 'keks', 'kamila']
+    term = request.args.get('term')
+    filtered_users = [i for i in users if term in i]
+    return render_template('users/index.html', users=filtered_users, search=term)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
